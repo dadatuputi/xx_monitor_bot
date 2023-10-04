d30f69bd3e2f4b589678283dbc3fad00e46ef766
dadatuputi <1217269+dadatuputi@users.noreply.github.com>
2023-10-04 21:08:45 +0100

    make componenents independently start

--- src/index.ts
+++ src/index.ts
@@ -16,19 +16,28 @@ const db: Database = await Database.connect(process.env.MONGO_URI);
 
 // start cmix cron
 // todo - consolidate /monitor commands into single command, then handle command loading like claim i.e. throw error when env vars aren't available. 
-await import('./env-guard/monitor.js')
-startPolling(db, process.env.CMIX_API_ENDPOINT!, process.env.CMIX_API_CRON!);
+(async () => {
+    await import('./env-guard/monitor.js')
+    startPolling(db, process.env.CMIX_API_ENDPOINT!, process.env.CMIX_API_CRON!);
+  }
+)();
 
 // start chain listener
-await import('./env-guard/claim.js')
-startAllClaiming(db, process.env.CHAIN_RPC_ENDPOINT!);
-startListeningCommission(process.env.CHAIN_RPC_ENDPOINT!);
+(async () => {
+    await import('./env-guard/claim.js')
+    startAllClaiming(db, process.env.CHAIN_RPC_ENDPOINT!);
+    startListeningCommission(process.env.CHAIN_RPC_ENDPOINT!);
+  }
+)();
 
 
 // start bots
 //  start discord.js
-await import('./env-guard/discord.js')
-initDiscord(db, process.env.DISCORD_TOKEN!);
+(async () => {
+    await import('./env-guard/discord.js')
+    initDiscord(db, process.env.DISCORD_TOKEN!);
+  }
+)();  
 
 // discord/index.js: loads all the event handlers for discord
 //  once discord.js client is ready, magic starts in events/ready.js
